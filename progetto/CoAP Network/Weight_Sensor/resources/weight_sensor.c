330ea2e83b50e72186fe20d7b77cbc0d8e84489c
giacomo pacini <giacomopacini98@gmail.com>
2021-07-17 18:40:22 +0200

    aiutatemi sto facendo dei danni

---
+++ progetto/CoAP Network/Weight_Sensor/resources/weight_sensor.c
@@ -0,0 +1,41 @@
+#include "contiki.h"
+#include "coap-engine.h"
+//#include "dev/leds.h"
+#include <string.h>
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+extern long current_weight;
+
+//static void res_post_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_weight_obs_handler(void);
+
+EVENT_RESOURCE(res_weight,
+		"title=\"weight value\";obs",
+		res_get_handler,
+		NULL,
+		NULL,
+		NULL,
+		res_weight_obs_handler);
+
+//risposta alla get standard
+static void
+res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	coap_set_header_content_format(response, APPLICATION_JSON);
+	snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"weight\": %ld, \"timestamp\": %lu, \"unit\": \"gram\"}", current_weight, clock_seconds());
+    coap_set_payload(response, (uint8_t *)buffer, strlen((char *)buffer));
+}
+
+//handler dell'observing
+static void res_weight_obs_handler(void)
+{
+	// Notify all the observers
+	// Before sending the notification the handler associated with the GET methods is called
+	//chiama la get qui
+	coap_notify_observers(&res_weight);
+}
