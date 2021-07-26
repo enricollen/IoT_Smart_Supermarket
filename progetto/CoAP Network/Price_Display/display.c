b41a0aeeaae829f5b6a0238bb8756b7107075465
giacomo pacini <giacomopacini98@gmail.com>
2021-07-26 19:27:40 +0200

    Collector CoAP connection bugfixes

--- progetto/CoAP Network/Price_Display/display.c
+++ progetto/CoAP Network/Price_Display/display.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "contiki.h"
 #include "coap-engine.h"
 #include "sys/etimer.h"
 #include <time.h>
@@ -15,14 +14,16 @@
 #include "net/ipv6/uip-debug.h"
 #include "routing/routing.h"
 
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "PriceDisplay"
+#define LOG_LEVEL LOG_LEVEL_APP
+
 #define SENSOR_TYPE "PriceDisplay"
 const char* node_name = "Price Display and CoAP Server";
 #include "../network_config.h"
 
-/* Log configuration */
-#include "sys/log.h"
-#define LOG_MODULE "App"
-#define LOG_LEVEL LOG_LEVEL_APP
+
 
 #define DEFAULT_PRICE 20.5
 #define MINIMUM_PRICE 0.90
@@ -48,6 +49,9 @@ void change_price(float updated_price){
   //saves the last refill timestamp
   last_price_change = clock_seconds();
 
+  //if you want to avoid error, please add a check to be sure that res_price is active!
+  res_price.trigger(); //calls obs_handler
+
 }
 
 bool check_price_validity(float price){
@@ -63,20 +67,45 @@ PROCESS_THREAD(contiki_ng_br_coap_server, ev, data){
 
   PROCESS_BEGIN();
 
+	coap_activate_resource(&res_price, "price");
+  //NOTE: you can call change_price only after the activation of res_price, because inside change_price there is a call to res_price.trigger();
   change_price(DEFAULT_PRICE);   //when the sensor is restarted, we reset the current price to DEFAULT PRICE
 
-	coap_activate_resource(&res_price, "price");
 
-  wait_connectivity();
+  //wait_connectivity();
+  etimer_set(&wait_connectivity_timer, CLOCK_SECOND* CONNECTION_TRY_INTERVAL);
+    while(!have_connectivity()){
+        PROCESS_WAIT_UNTIL(etimer_expired(&wait_connectivity_timer));
+        etimer_reset(&wait_connectivity_timer);
+    }
+  LOG_DBG("[%.*s]: Successfully connected to network\n");
+  //
 
   if(!initialize_node_id())
-    LOG_ERR("[%.*s]: Unable to initialize Node ID", node_name);
+    LOG_ERR("[%.*s]: Unable to initialize Node ID\n", node_name);
 
-  LOG_DBG("[%.*s]: up and running", node_name);
+  LOG_DBG("[%.*s]: up and running\n", node_name);
 
-  register_to_collector();
+  //register_to_collector();
+  etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
 
-  LOG_DBG("[%.*s]: Registration to Collector succeded", node_name);
+  while(!registered){
+		
+		coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);	
+
+		coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+		coap_set_header_uri_path(request, service_url);
+			
+		coap_set_payload(request, (uint8_t *)SENSOR_TYPE, sizeof(SENSOR_TYPE) - 1);
+    //TO DO: it will be useful to make a non blocking request and / or decrease the CoAP connection timeout
+		COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+    
+		etimer_reset(&wait_registration);
+    PROCESS_WAIT_UNTIL(etimer_expired(&wait_registration));
+  }
+  //
+
+  LOG_DBG("[%.*s]: Registration to Collector succeded\n", node_name);
 		
   PROCESS_END();
 }
--- progetto/CoAP Network/network_config.h
+++ progetto/CoAP Network/network_config.h
@@ -1,3 +1,4 @@
+#include "contiki.h"
 #include "coap-blocking-api.h"
 
 //#include "routing/routing.h"
@@ -9,15 +10,20 @@
 #define REGISTRATION_SUCCESSFULL "Registration Successfull"
 #define ALREADY_REGISTERED "Already Registered"
 #define WRONG_PAYLOAD "Invalid Sensor Type"
+#define INTERNAL_SERVER_ERROR "Internal error while handling the request"
 
 static struct etimer wait_connectivity_timer;
 static struct etimer wait_registration;
+
+//static struct ctimer ct_wait_registration;
+//void *ct_ptr_w_t = &ct_wait_registration; //maybe this should be just before the call to ctimer_set
+
 static bool registered = false;
 static char* service_url = "/register";
 static coap_endpoint_t server_ep;
 static coap_message_t request[1]; /* This way the packet can be treated as pointer as usual. */
 
-#define CLIENT_ID_SIZE 6
+#define CLIENT_ID_SIZE 6 + 1 //becaus of \0
 char sensor_id[CLIENT_ID_SIZE];
 
 static bool have_connectivity(void)
@@ -29,14 +35,21 @@ static bool have_connectivity(void)
   return true;
 }
 
-void wait_connectivity(){
+/*
+inline void wait_connectivity(){
     etimer_set(&wait_connectivity_timer, CLOCK_SECOND* CONNECTION_TRY_INTERVAL);
     while(!have_connectivity()){
-        PROCESS_WAIT_UNTIL(etimer_expired(&wait_connectivity_timer));
-        etimer_reset(&wait_connectivity_timer);
+        //Important NOTE: we cannot use the macro PROCESS_WAIT_UNTIL inside a function, beacause in this way the compiler does not see the process_pt and raises an error
+          //src: https://groups.google.com/g/osdeve_mirror_rtos_contiki-developers/c/XdOXnXsgj38
+        //PROCESS_WAIT_UNTIL(etimer_expired(&wait_connectivity_timer));
+        //etimer_reset(&wait_connectivity_timer);
+        //searching online it seems that the best way to achieve this is to use a ctimer
+          //new note: the ctimer does not fit our case because we want to block the execution of the process, we do not want it to go to the next state (when the network is ok)
+            //the best way we've found is to copy paste this code directly in the process_Thread
     }
     LOG_DBG("[%.*s]: Successfully connected to network");
 }
+*/
 
 bool initialize_node_id(){
     if(!have_connectivity())
@@ -47,12 +60,58 @@ bool initialize_node_id(){
     return true;
 }
 
-void client_chunk_handler(coap_message_t *response)
+void client_chunk_handler(coap_message_t * response) //void* instead of coap_message_t*
+{
+	const uint8_t *chunk = NULL;
+	if(response == NULL) {
+		LOG_INFO("Request timed out\n");
+		//etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
+		return;
+	}
+	
+	int len = coap_get_payload(response, &chunk);
+	LOG_INFO("[client_chunk_handler]: just called coap_get_payload. len = %d | payload: %s\n", len, (char * ) chunk);
+  
+	if(strncmp((char*)chunk, REGISTRATION_SUCCESSFULL, len) == 0){ 
+		registered = true;
+	}
+    else if(strncmp((char*)chunk, ALREADY_REGISTERED, len) == 0){
+        registered = true;
+    }
+    else if(strncmp((char*)chunk, WRONG_PAYLOAD, len) == 0){
+        registered = false;
+        LOG_ERR("[network_config: client_chunk_handler] Invalid node type\n");
+    }else if(strncmp((char*)chunk, INTERNAL_SERVER_ERROR, len) == 0){
+        registered = false;
+        LOG_ERR("[network_config: client_chunk_handler] Collector internal error!\n");
+    }else{
+      LOG_WARN("[client_chunk_handler]: unrecognized response from Collector\n");
+      registered = false;
+    }
+}
+/*
+DOES NOT WORK BECAUSE PROCESS_WAIT_UNTIL CANNOT BE CALLED INSIDE A FUNCTION
+void register_to_collector(){
+    while(!registered){
+		
+		coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);	
+
+		coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+		coap_set_header_uri_path(request, service_url);
+			
+		coap_set_payload(request, (uint8_t *)SENSOR_TYPE, sizeof(SENSOR_TYPE) - 1);
+		COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+		
+    PROCESS_WAIT_UNTIL(etimer_expired(&wait_registration));
+  }
+}
+
+void ctimer_client_chunk_handler(coap_message_t *response)
 {
 	const uint8_t *chunk;
 	if(response == NULL) {
 		LOG_INFO("Request timed out\n");
-		etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
+		//etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
 		return;
 	}
 	
@@ -69,11 +128,11 @@ void client_chunk_handler(coap_message_t *response)
         registered = false;
         LOG_ERR("[network_config: client_chunk_handler] Invalid node type\n");
     }  
-    else
-		etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
+    //else
+		//etimer_set(&wait_registration, CLOCK_SECOND* REGISTRATION_TRY_INTERVAL);
 }
-
-void register_to_collector(){
+DOES NOT WORK BECAUSE COAP_BLOCKING_REQUEST CANNOT BE CALLED INSIDE A FUNCTION
+void ctimer_register_to_collector(void* ctimer){
     while(!registered){
 		
 		coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);	
@@ -82,9 +141,14 @@ void register_to_collector(){
 		coap_set_header_uri_path(request, service_url);
 			
 		coap_set_payload(request, (uint8_t *)SENSOR_TYPE, sizeof(SENSOR_TYPE) - 1);
-		COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+		COAP_BLOCKING_REQUEST(&server_ep, request, ctimer_client_chunk_handler);
 		
-        PROCESS_WAIT_UNTIL(etimer_expired(&wait_registration));
+    struct ctimer * c_timer_ptr = ctimer;
+
+    ctimer_reset(c_timer_ptr);
   }
 }
-
+USE CASE EXAMPLE:
+//INSIDE PROCESS_THREAD:
+ctimer_set(&ct_wait_registration, CLOCK_SECOND * REGISTRATION_TIME_INTERVAL, c_timer_register_to_collector, ct_ptr_w_t);
+*/
